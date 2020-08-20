af0e016a09361f821e4534ff31096dd553615a4f
Saurabh <saurabhdevle@gmail.com>
2020-08-20 12:13:58 +0530

    Add files via upload

---
+++ Day4/Assignment4.java
@@ -0,0 +1,16 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the Number : ");
+		long input = scan.nextLong();
+		System.out.println("Input number "+input);
+
+	}
+
+}
